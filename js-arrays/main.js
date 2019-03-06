644a7f5790e116ad269ca485ab9e836f4e4c6288
Austin Casey <thepoast@gmail.com>
2019-03-05 19:30:31 -0600

    midclasscommit

--- js-arrays/main.js
+++ js-arrays/main.js
@@ -1,1 +1,37 @@
-console.log('hello');
+
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+};
+
+
+const dinosaurs = [
+
+    {
+        dinoType: 'Archaeopteryx'
+    },
+    {
+        dinoType: 'Pterodactyl'
+    },
+    {
+        dinoType: 'Megalodon'
+    }
+];
+
+//loop over dinosaurs
+//build up a domstring
+//print domstring to the dom
+
+const buildDinosaurs=()=>{
+    let domString = '';
+    for(let i = 0; i < dinosaurs.length; i++){
+        domString += `<h3>${dinosaurs[i].dinoType}</h3>`;
+    }
+    printToDom('dino-barn', domString);
+};
+
+const init =()=>{
+    buildDinosaurs();
+
+};
+init();
